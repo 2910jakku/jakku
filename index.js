f985be07aa0a59fedd47597f95ee38e1611c73da
danieltwcom <danieltwcom@hotmail.com>
2017-05-13 16:55:35 -0700

    order and kitchen backend

--- index.js
+++ index.js
@@ -38,51 +38,13 @@ app.get("/", function(req, resp){
     resp.sendFile(pF+"/order.html");
 });
 
-//kitchen page
-app.get("/kitchen", function(req, resp){
-    resp.sendFile(pF+"/kitchen.html");
+app.get("/board",function(req,resp){
+   resp.sendFile(pF+"/order_board.html"); 
 });
 
-//kitchen login page
-app.get("/kitchenlogin", function(req, resp){
-    resp.sendFile(pF+"/kitchen_login.html");
+app.get("/kitchen",function(req,resp){
+   resp.sendFile(pF+"/kitchen.html");  
 });
-
-/*-----------------------------KITCHEN LOGIN---------------------------*/
-//kitchen login
-app.post("/kitchenlogin", function(req, resp){
-    var username = req.body.username;
-    var password = req.body.password;
-    
-    pg.connect(dbURL, function(err, client, done){
-        if(err){
-            console.log(err);
-            resp.end("FAIL");
-        }
-        
-        client.query("SELECT username, id FROM employee WHERE username = $1 AND password = $2", [username, password], function(err, result){
-            done();
-            if(err){
-                console.log(err);
-                resp.end("FAIL");
-            }
-            
-            if(result.rows.length > 0){
-                req.session.username = result.rows[0].username;
-                req.session.id = result.rows[0].id;
-                var obj = {
-                    status:"success"
-                }
-                
-                resp.send(obj);
-            } else {
-                resp.end("FAIL");
-            }
-        })
-    });
-});
-/*-----------------------------KITCHEN LOGIN ENDS---------------------------*/
-
 
 app.post("/order",function(req,resp){
     // read menu from db
@@ -97,6 +59,7 @@ app.post("/order",function(req,resp){
         var order_item_id = req.body.order_item_id;
         var order_quantity = req.body.order_quantity;
         var NumberRegEx = /^[1-9][0-9]{0,2}?$/;
+        var valid = true;
         
         // input validation
         for(var i=0;i<order_item_id.length;i++){
@@ -106,44 +69,101 @@ app.post("/order",function(req,resp){
                 var obj = {
                     status:"input quantity error"
                 }
+                valid = false;
                 resp.send(obj);
-            }
-            
-            // test both array match same length
-            if(order_item_id.length != order_quantity.length){
-                var obj = {
-                    status:"input not match"
+            }else{
+                // test both array match same length
+                if(order_item_id.length != order_quantity.length){
+                    var obj = {
+                        status:"input not match"
+                    }
+                    valid = false;
+                    resp.send(obj);
                 }
-                resp.send(obj);
             }
         }
         
         // place the order
-        var order_id;
-        var query = "INSERT INTO orders (order_status) VALUES (0) RETURNING id";
-        runQuery(query,function(result){
-            order_id = result[0].id;
-            for(var i=0;i<order_item_id.length;i++){
-                var item_id = order_item_id[i];
-                var quantity = order_quantity[i];
+        if(valid){
+            var order_id;
+            var query = "INSERT INTO orders (order_status) VALUES (0) RETURNING id,order_status,order_date";
+            runQuery(query,function(result){
+                order_id = result[0].id;
+                for(var i=0;i<order_item_id.length;i++){
+                    var item_id = order_item_id[i];
+                    var quantity = order_quantity[i];
 
-                var query2 = "INSERT INTO order_details (order_number,food_number,quantity,status) VALUES ("+order_id+","+item_id+","+quantity+","+"0"+")";
-                console.log(query2);
-                runQuery(query2,function(result){
-    
-                });
-            }
-            var obj = {
-                status:"order successfully placed",
-                order_id:order_id
-            }
-            resp.send(obj);
-        });
+                    var query2 = "INSERT INTO order_details (order_number,food_number,quantity,status) VALUES ("+order_id+","+item_id+","+quantity+","+"0"+") RETURNING order_number , id,(select name from food where id = "+item_id+"),quantity,status";
+                    console.log(query2);
+                    runQuery(query2,function(result){
+                        io.sockets.emit("add order details",result)
+                    });
+                }
+                var obj = {
+                    status:"order successfully placed",
+                    order_id:order_id
+                }
+                io.sockets.emit("add order",result);
+                resp.send(obj);
+            });
+        }
+        
     }
 });
 
 app.post("/board",function(req,resp){
-    
+    if(req.body.type == "read order status"){
+        query = "SELECT * FROM orders ORDER BY id ASC";
+        runQuery(query,function(result){
+            resp.send(result);
+        })
+    }
+});
+
+app.post("/kitchen",function(req,resp){
+    if(req.body.type == "read order detail"){
+        query = "SELECT order_details.id,order_details.order_number,food.name,order_details.quantity,order_details.status from order_details LEFT JOIN food ON order_details.food_number = food.id WHERE status = 0 ORDER BY order_details.order_number ASC";
+        runQuery(query,function(result){
+           resp.send(result);
+        });
+    }
+});
+
+// socket 
+io.on("connection",function(socket){
+    socket.on("order detail done",function(obj){
+        var order_detail_id = obj.order_detail_id;
+        console.log(order_detail_id);
+        
+        // database update on order_detail table 
+        var query = "UPDATE order_details SET status=1 WHERE id="+order_detail_id+" RETURNING order_number";
+        runQuery(query,function(result){
+            order_number = result[0].order_number;
+            var query2 = "SELECT * FROM order_details WHERE order_number="+order_number;
+            console.log(query2);
+            runQuery(query2,function(result){
+                var order_status = true;
+                for(var i=0;i<result.length;i++){
+                    if(result[i].status==0){
+                        order_status = false;
+                    }
+                }
+                if(order_status){
+                    var query3 = "UPDATE orders SET order_status = 1 WHERE id ="+order_number+" RETURNING id";
+                    console.log(query3);
+                    runQuery(query3,function(result){
+                        console.log(result[0].id);
+                        id = result[0].id;
+                        io.sockets.emit("update board",id);
+                    });
+                }
+
+            })
+            socket.emit("update order detail",obj);
+        });
+        
+        
+    });
 });
 
 // run query
@@ -161,6 +181,7 @@ function runQuery(myQuery,callback){
             }
             console.log(result.rows);
             callback(result.rows);
+            return true;
         });
     });
 }
