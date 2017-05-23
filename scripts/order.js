efc331d86298c5cf87cfac9d9198d821f203102d
danieltwcom <danieltwcom@hotmail.com>
2017-05-23 14:05:24 -0700

    add test

--- scripts/order.js
+++ scripts/order.js
@@ -5,9 +5,65 @@ var statusButton = document.getElementById("statusButton");
 var order_item_id = [];
 var order_quantity = [];
 
+// when page loaded
 $(document).ready(function(){
     clearOrderList();
     addEvent();
+});
+    
+// send request to get the menu 
+function documentReady(callback){
+    $.ajax({
+        url:"/order",
+        type:"post",
+        data:{
+            type:"read menu"
+        },
+       success:function(resp){
+           console.log(resp);
+           for(var i=0;i<resp.length;i++){
+                ndiv = document.createElement("div");
+                ndiv.innerHTML = "<div><img src='"+resp[i].url+"'></div>";
+                ndiv.innerHTML = "<div id='itemName'> Name: " + resp[i].name; + "</div>";
+                ndiv.innerHTML += "<div id='itemPrice'> Price: $" + resp[i].price; + "</div>";
+                ndiv.innerHTML += "<div id='itemQuantity'> Quantity: <input type='text' value = '1' id='itemQuantity' placeholder ='enter number'></div>";
+                ndiv.innerHTML += "<button type='button' class='addBtn'>add</button>";
+               
+                ndiv.id = "food_item"+resp[i].id;
+                menu.appendChild(ndiv);
+           }
+           
+           callback(resp);
+       }
+   });
+}
+
+// add event listener to all buttons
+function addEvent(){
+    documentReady(function(food_list){
+        // "add" button to add item to a list
+        var addBtn = document.getElementsByClassName("addBtn");
+        console.log(addBtn);
+        for(var i=0;i<addBtn.length;i++){
+            addBtn[i].addEventListener("click",function(){
+
+                var parent_div_id = this.parentNode.id;
+                var item_id = parseInt(parent_div_id[parent_div_id.length-1]);
+
+                var quantity = $("#"+parent_div_id+" #itemQuantity")[1].value;
+                console.log(quantity);
+                var item_name = food_list[item_id - 1].name;
+
+                order_list.innerHTML += "<div>Name: "+item_name+ " Quantity: " +quantity +"</div>";
+
+                order_item_id.push(item_id);
+                order_quantity.push(quantity);
+            });
+        }
+        
+    });
+    
+    // "place order" button action
     placeButton.addEventListener("click",function(){
         console.log("clicked");
         if(order_item_id.length < 1){
@@ -34,61 +90,14 @@ $(document).ready(function(){
         }
         
     });
+    
+    // "view status" button to view order status
     statusButton.addEventListener("click",function(){
         location.href = "/board";
     });
-});
-    
-
-function documentReady(callback){
-    $.ajax({
-        url:"/order",
-        type:"post",
-        data:{
-            type:"read menu"
-        },
-       success:function(resp){
-           console.log(resp);
-           for(var i=0;i<resp.length;i++){
-                ndiv = document.createElement("div");
-                ndiv.innerHTML = "<div id='itemName'> Name: " + resp[i].name; + "</div>";
-                ndiv.innerHTML += "<div id='itemPrice'> Price: $" + resp[i].price; + "</div>";
-                ndiv.innerHTML += "<div id='itemQuantity'> Quantity: <input type='text' value = '1' id='itemQuantity' placeholder ='enter number'></div>";
-                ndiv.innerHTML += "<button type='button' class='addBtn'>add</button>";
-               
-                ndiv.id = "food_item"+resp[i].id;
-                menu.appendChild(ndiv);
-           }
-           
-           callback(resp);
-       }
-   });
 }
 
-function addEvent(){
-    documentReady(function(food_list){
-        var addBtn = document.getElementsByClassName("addBtn");
-        console.log(addBtn);
-        for(var i=0;i<addBtn.length;i++){
-            addBtn[i].addEventListener("click",function(){
-
-                var parent_div_id = this.parentNode.id;
-                var item_id = parseInt(parent_div_id[parent_div_id.length-1]);
-
-                var quantity = $("#"+parent_div_id+" #itemQuantity")[1].value;
-                console.log(quantity);
-                var item_name = food_list[item_id - 1].name;
-
-                order_list.innerHTML += "<div>Name: "+item_name+ " Quantity: " +quantity +"</div>";
-
-                order_item_id.push(item_id);
-                order_quantity.push(quantity);
-            });
-        }
-        
-    });
-}
-
+// clear the item list of the order
 function clearOrderList(){
     order_item_id = [];
     order_quantity = [];
--- scripts/order_board.js
+++ scripts/order_board.js
@@ -22,7 +22,9 @@ function initSockets(){
     socket = io();
 }
 
+// socket actions
 function socketHandle(){
+    // when order stats change
     socket.on("update board",function(order_id){
         var node = document.getElementById(order_id).childNodes;
         console.log(node);
@@ -30,12 +32,14 @@ function socketHandle(){
         node[1].innerHTML = "1";
     });
     
+    // new order placed
     socket.on("add order",function(result){
         console.log(result)
        loadOrderDetails(result);
     });
 }
 
+// load order details from db
 function loadOrderDetails(resp){
     for(var i=0;i<resp.length;i++){
         var time = resp[i].order_date.replace(/T/, ' ').replace(/\..+/, '');
